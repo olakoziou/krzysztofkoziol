7cd8cd2cabb6c6aadc1f48e313148f9e551647c2
Ola Kozioł <o.stanislawska92@gmail.com>
2021-10-27 10:15:00 +0200

    add cookie bar

--- frontend/src/components/Background.js
+++ frontend/src/components/Background.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { colors } from '../styles';
 import BackBtn from './backBtn/BackBtn';
+import CookieConsent from 'react-cookie-consent';
 
 const Box = styled.div`
   background: ${(props) =>
@@ -16,6 +17,28 @@ function Background({ children }) {
     <Box navy1={color.navy1} navy2={color.navy2} navy3={color.navy3}>
       {children}
       <BackBtn />
+      <CookieConsent
+        buttonText="Ok! :)"
+        enableDeclineButton
+        declineButtonText="Nie ok :( opuszczam stronę."
+        flipButtons
+        location="bottom"
+        cookieName="myAwesomeCookieName2"
+        // visible="show"
+        style={{
+          background: '#2B373B',
+          width: '100%',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          alignItems: 'center',
+          fontSize: '18px',
+        }}
+        buttonStyle={{ color: '#4e503b', fontSize: '13px' }}
+        expires={150}
+      >
+        Cześć! Dla poprawnego działania, strona ta wykorzystuje pliki cookie.
+        Ok?{' '}
+      </CookieConsent>
     </Box>
   );
 }
