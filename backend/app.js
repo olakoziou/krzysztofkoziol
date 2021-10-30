9321f8fc871556846a321619e5e732a8c6af7823
Ola Kozioł <o.stanislawska92@gmail.com>
2021-10-30 23:04:32 +0200

    heroku7

--- backend/app.js
+++ backend/app.js
@@ -23,18 +23,18 @@ if (process.env.NODE_ENV === 'development') {
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-});
-
 //ROUTES
 app.use('/api/trainings', trainingRouter);
 app.use('/api/gallery', galleryRouter);
 app.use('/api/videos', videoRouter);
 app.use('/api/kup-szkolenie', buyTrainingRouter);
 app.use('/api/send-email', sendEmailRouter);
+
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
