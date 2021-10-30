fd0238d7c7c96ebfb437bc3ee3bc6082530b26aa
Ola Kozioł <o.stanislawska92@gmail.com>
2021-10-30 22:22:16 +0200

    heroku try

--- backend/app.js
+++ backend/app.js
@@ -23,10 +23,10 @@ if (process.env.NODE_ENV === 'development') {
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../static')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../static', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
 });
 
 //ROUTES
