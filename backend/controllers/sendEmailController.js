c033963dc804891fc4730a62f2af3b4de8da4a29
Ola Kozioł <o.stanislawska92@gmail.com>
2022-01-06 18:26:42 +0100

    final changes

--- backend/controllers/sendEmailController.js
+++ backend/controllers/sendEmailController.js
@@ -14,14 +14,11 @@ exports.sendEmail = async (req, res, next) => {
     },
   });
 
-  try {
-    await transport.sendMail(
-      {
-        from: process.env.MAIL_FROM,
-        to: req.body.data.email,
-        subject:
-          'Zarabianie na nieruchomościach. Szkolenie i warsztat praktyczny.',
-        html: `<div className="email" style="
+  await transport.sendMail({
+    from: process.env.MAIL_FROM,
+    to: req.body.data.email,
+    subject: 'Zarabianie na nieruchomościach. Szkolenie i warsztat praktyczny.',
+    html: `<div className="email" style="
               padding: 10px 5px;
               font-family: sans-serif;
               line-height: 1.6;
@@ -49,8 +46,8 @@ exports.sendEmail = async (req, res, next) => {
                     font-family: sans-serif;
                     list-style: none
                     ">Adres: ${req.body.data.address1}, ${
-          req.body.data.address2
-        }, ${req.body.data.address3},</li>
+      req.body.data.address2
+    }, ${req.body.data.address3},</li>
                 <li style="
                     font-family: sans-serif;
                     list-style: none
@@ -86,111 +83,98 @@ exports.sendEmail = async (req, res, next) => {
               <p>Po zaksięgowaniu płatności, faktura zostanie wysłana na Twój adres mailowy.</p>
               <p>Jeśli masz jakieś pytania, zadzwoń - <a href="tel:+48-796-483-333">796-483-333.</a> Pozdrawiam, Krzysiek Kozioł.</p>
               <div>
-              <p><a href="https://krzysztofkoziol.pl/" target="_blank">www.krzysztofkoziol.pl</a></p>
-              <p><a href="https://sunstreetinvestment.pl/" target="_blank">www.sunstreetinvestment.pl</a></p>
-              <p><a href="https://narusa.pl/" target="_blank">www.narusa.pl</a></p>
+              <p style="margin: 0"><a href="https://krzysztofkoziol.pl/" target="_blank">www.krzysztofkoziol.pl</a></p>
+              <p style="margin: 0"><a href="https://sunstreetinvestment.pl/" target="_blank">www.sunstreetinvestment.pl</a></p>
+              <p style="margin: 0"><a href="https://narusa.pl/" target="_blank">www.narusa.pl</a></p>
               </div>
                </div>
           `,
-      },
-      (err, info) => {
-        if (err) {
-          console.log(err);
-        } else if (info) {
-          console.log(info.messageId);
+  });
+
+  await transport.sendMail({
+    from: process.env.MAIL_FROM,
+    to: process.env.MAIL_KRIS,
+    subject: 'SZKOLENIE - pojawił się nowy klient.',
+    html: `<div className="email" style="
+          padding: 10px 5px;
+          font-family: sans-serif;
+          line-height: 2;
+          font-size: 20px;
+          ">
+          <h2>Zarabianie na nieruchomościach. Szkolenie i warsztat praktyczny.</h2>
+          <p>Pojawił się nowy klient. Dane: </p>
+          <ul>
+            ${
+              req.body.data.name
+                ? `<li>Imię i nazwisko: ${req.body.data.name}</li>`
+                : `<li>Firma: ${req.body.data.company}</li>`
+            }
+            <li>Email: ${req.body.data.email}</li>
+            <li>Adres: ${req.body.data.address1}, ${req.body.data.address2}, ${
+      req.body.data.address3
+    },</li>
+  <li>NIP: ${req.body.data.nip}</li>
+            <p>Czekaj na płatność. Sprawdź swoje konto bankowe.</p>
+          </ul>
+           </div>
+      `,
+  });
+
+  await transport.sendMail({
+    from: process.env.MAIL_FROM,
+    to: process.env.MAIL_SSI,
+    subject: 'SZKOLENIE - pojawił się nowy klient.',
+    html: `<div className="email" style="
+      padding: 10px 5px;
+      font-family: sans-serif;
+      line-height: 2;
+      font-size: 20px;
+      ">
+      <h2>Zarabianie na nieruchomościach. Szkolenie i warsztat praktyczny.</h2>
+      <p>Pojawił się nowy klient. Dane: </p>
+      <ul>
+        ${
+          req.body.data.name
+            ? `<li>Imię i nazwisko: ${req.body.data.name}</li>`
+            : `<li>Firma: ${req.body.data.company}</li>`
         }
-      }
-    );
-  } catch (err) {
-    console.log(err);
-  }
+        <li>Email: ${req.body.data.email}</li>
+        <li>Adres: ${req.body.data.address1}, ${req.body.data.address2}, ${
+      req.body.data.address3
+    },</li>
+  <li>NIP: ${req.body.data.nip}</li>
+        <p>Czekaj na płatność. Sprawdź swoje konto bankowe.</p>
+      </ul>
+       </div>
+  `,
+  });
 
-  // await transport.sendMail({
-  //   from: process.env.MAIL_FROM,
-  //   to: process.env.MAIL_KRIS,
-  //   subject: 'SZKOLENIE - pojawił się nowy klient.',
-  //   html: `<div className="email" style="
-  //         padding: 10px 5px;
-  //         font-family: sans-serif;
-  //         line-height: 2;
-  //         font-size: 20px;
-  //         ">
-  //         <h2>Zarabianie na nieruchomościach. Szkolenie i warsztat praktyczny.</h2>
-  //         <p>Pojawił się nowy klient. Dane: </p>
-  //         <ul>
-  //           ${
-  //             req.body.data.name
-  //               ? `<li>Imię i nazwisko: ${req.body.data.name}</li>`
-  //               : `<li>Firma: ${req.body.data.company}</li>`
-  //           }
-  //           <li>Email: ${req.body.data.email}</li>
-  //           <li>Adres: ${req.body.data.address1}, ${req.body.data.address2}, ${
-  //     req.body.data.address3
-  //   },</li>
-  // <li>NIP: ${req.body.data.nip}</li>
-  //           <p>Czekaj na płatność. Sprawdź swoje bankowe.</p>
-  //         </ul>
-  //          </div>
-  //     `,
-  // });
-
-  // await transport.sendMail({
-  //   from: process.env.MAIL_FROM,
-  //   to: process.env.MAIL_SSI,
-  //   subject: 'SZKOLENIE - pojawił się nowy klient.',
-  //   html: `<div className="email" style="
-  //     padding: 10px 5px;
-  //     font-family: sans-serif;
-  //     line-height: 2;
-  //     font-size: 20px;
-  //     ">
-  //     <h2>Zarabianie na nieruchomościach. Szkolenie i warsztat praktyczny.</h2>
-  //     <p>Pojawił się nowy klient. Dane: </p>
-  //     <ul>
-  //       ${
-  //         req.body.data.name
-  //           ? `<li>Imię i nazwisko: ${req.body.data.name}</li>`
-  //           : `<li>Firma: ${req.body.data.company}</li>`
-  //       }
-  //       <li>Email: ${req.body.data.email}</li>
-  //       <li>Adres: ${req.body.data.address1}, ${req.body.data.address2}, ${
-  //     req.body.data.address3
-  //   },</li>
-  // <li>NIP: ${req.body.data.nip}</li>
-  //       <p>Czekaj na płatność. Sprawdź swoje konto bankowe.</p>
-  //     </ul>
-  //      </div>
-  // `,
-  // });
-
-  //   await transport.sendMail({
-  //     from: process.env.MAIL_FROM,
-  //     to: process.env.MAIL_OLA,
-  //     subject: 'SZKOLENIE - pojawił się nowy klient.',
-  //     html: `<div className="email" style="
-  //     padding: 10px 5px;
-  //     font-family: sans-serif;
-  //     line-height: 2;
-  //     font-size: 20px;
-  //     ">
-  //     <h2>Zarabianie na nieruchomościach. Szkolenie i warsztat praktyczny.</h2>
-  //     <p>Pojawił się nowy klient. Dane: </p>
-  //     <ul>
-  //       ${
-  //         req.body.data.name
-  //           ? `<li>Imię i nazwisko: ${req.body.data.name}</li>`
-  //           : `<li>Firma: ${req.body.data.company}</li>`
-  //       }
-  //       <li>Email: ${req.body.data.email}</li>
-  //       <li>Adres: ${req.body.data.address1}, ${req.body.data.address2}, ${
-  //       req.body.data.address3
-  //     },</li>
-  //     <li>NIP: ${req.body.data.nip}</li>
-  //       <p>Czekaj na płatność. Sprawdź swoje konto bankowe.</p>
-  //     </ul>
-  //      </div>
-  // `,
-  //   });
-
-  3;
+  await transport.sendMail({
+    from: process.env.MAIL_FROM,
+    to: process.env.MAIL_OLA,
+    subject: 'SZKOLENIE - pojawił się nowy klient.',
+    html: `<div className="email" style="
+      padding: 10px 5px;
+      font-family: sans-serif;
+      line-height: 2;
+      font-size: 20px;
+      ">
+      <h2>Zarabianie na nieruchomościach. Szkolenie i warsztat praktyczny.</h2>
+      <p>Pojawił się nowy klient. Dane: </p>
+      <ul>
+        ${
+          req.body.data.name
+            ? `<li>Imię i nazwisko: ${req.body.data.name}</li>`
+            : `<li>Firma: ${req.body.data.company}</li>`
+        }
+        <li>Email: ${req.body.data.email}</li>
+        <li>Adres: ${req.body.data.address1}, ${req.body.data.address2}, ${
+      req.body.data.address3
+    },</li>
+      <li>NIP: ${req.body.data.nip}</li>
+        <p>Czekaj na płatność. Sprawdź swoje konto bankowe.</p>
+      </ul>
+       </div>
+  `,
+  });
 };
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5001',
+      target: 'https://krzysztofkoziol.pl',
       changeOrigin: true,
     })
   );
